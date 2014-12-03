af47058a36237abbd0b7a1c9e2d70d9a25e2fbc2
Kian Jones <kian.jones95@gmail.com>
2014-12-02 23:04:02 -0500

    Update code2040.py

--- code2040.py
+++ code2040.py
@@ -4,7 +4,7 @@ import dateutil.parser
 import requests as req
 import sys
 
-#registration 'https://github.com/KianJones/code2040' 'kjones43@u.rochester.edu'
+#registration 
 
 jbody = json.dumps({'github': sys.argv[1], 'email': sys.argv[2]})
 
@@ -26,22 +26,26 @@ def submit_answer(url,body):
     print answer.text
     return answer
 
-#stage 1
+# stage 1
 
 question1 = get_question("http://challenge.code2040.org/api/getstring")
-
 jdata1 = json.loads(question1.text)
+
+# indexing strings uses [start:end:step]
+# setting step to negative -1 reverses the string
 revstring = jdata1['result'][::-1]
+
 jbody1 = json.dumps({'token':'sO3XxG9cqZ','string':revstring})
 
 answer1 = submit_answer("http://challenge.code2040.org/api/validatestring",jbody1)
 
 
-#stage 2
+# stage 2
 
 question2 = get_question("http://challenge.code2040.org/api/haystack")
 jdata2 = json.loads(question2.text)
 
+# linear search, because we have to traverse each element of the list
 i = 0
 for item in jdata2['result']['haystack']:
     if item == jdata2['result']['needle']:
@@ -52,11 +56,12 @@ for item in jdata2['result']['haystack']:
 jbody2 = json.dumps({'token':token,'needle':i})
 answer2 = submit_answer("http://challenge.code2040.org/api/validateneedle",jbody2)
 
-#stage3
+# stage3
 
 question3 = get_question("http://challenge.code2040.org/api/prefix")
 jdata3 = json.loads(question3.text)
 
+# slice the string form beginning to prefix and see if it equals prefix
 result = []
 prefix = jdata3['result']['prefix']
 for item in jdata3['result']['array']:
@@ -67,15 +72,17 @@ for item in jdata3['result']['array']:
 jbody3 = json.dumps({'token':token,'array':result})
 answer3 = submit_answer("http://challenge.code2040.org/api/validateprefix",jbody3)
 
-#stage 4
+# stage 4
 question4 = get_question("http://challenge.code2040.org/api/time")
 jdata4 = json.loads(question4.text)
 
+# use dateutil to add the seconds to the startdate
 start_date = dateutil.parser.parse(jdata4['result']['datestamp'])
 end_date = sdate + datetime.timedelta(seconds=jdata4['result']['interval'])
 
 jbody4 = json.dumps({'token':token, 'datestamp':edate.isoformat()})
 answer4 = submit_answer("http://challenge.code2040.org/api/validatetime",jbody4)
 
-responsea = req.post("http://challenge.code2040.org/api/status",data=jbody1)
-print responsea.text
+# and we're done
+grades = req.post("http://challenge.code2040.org/api/status",data=jbody1)
+print grades.text
